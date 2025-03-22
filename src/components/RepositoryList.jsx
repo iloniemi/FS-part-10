f70c231d7ffd5fb61945a050cd3f13571880b080
iloniemi <juho.iloniemi@gmail.com>
2025-03-22 12:41:06 +0200

    before graphql

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import theme from '../theme';
+import { useEffect, useState } from 'react';
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -11,7 +13,7 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.background,
   },
 });
-
+/*
 const repositories = [
   {
     id: 'jaredpalmer.formik',
@@ -58,14 +60,20 @@ const repositories = [
     ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
   },
 ];
-
+*/
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={ RepositoryItem }
       style={ styles.container }
